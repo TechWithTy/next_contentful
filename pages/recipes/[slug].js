dc2b2e2330f2eccdf0df5b131743211643926342
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-08-12 18:19:58 -0600

    ISR-

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,5 +1,5 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { createClient } from 'contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
 
 const client = createClient({
@@ -14,18 +14,17 @@ export const getStaticPaths = async () => {
 
   const paths = res.items.map((item) => {
     return {
-      params: {
-        slug: item.fields.slug,
-      },
+      params: { slug: item.fields.slug },
     };
   });
+
   return {
-    paths: paths,
+    paths,
     fallback: false,
   };
 };
 
-export async function getStaticProps({ params }) {
+export const getStaticProps = async ({ params }) => {
   const { items } = await client.getEntries({
     content_type: 'recipe',
     'fields.slug': params.slug,
@@ -33,12 +32,14 @@ export async function getStaticProps({ params }) {
 
   return {
     props: { recipe: items[0] },
-    revalidate: 1
+    revalidate: 1,
   };
-}
+};
+
 export default function RecipeDetails({ recipe }) {
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
+  console.log(method);
 
   return (
     <div>
@@ -50,18 +51,21 @@ export default function RecipeDetails({ recipe }) {
         />
         <h2>{title}</h2>
       </div>
-      <div className="info">
-        <p>Takes about {cookingTime} mins to cook</p>
-        <h3>Ingredients</h3>
-        {ingredients.map((ing, index) => {
-          return <span key={index}>{ing}</span>;
-        })}
 
-        <div className="method">
-          <h3>Method:</h3>
-          <div>{documentToReactComponents(method)}</div>
-        </div>
+      <div className="info">
+        <p>Takes about {cookingTime} mins to cook.</p>
+        <h3>Ingredients:</h3>
+
+        {ingredients.map((ing) => (
+          <span key={ing}>{ing}</span>
+        ))}
       </div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
       <style jsx>{`
         h2,
         h3 {
